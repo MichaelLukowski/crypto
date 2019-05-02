e7586cddfe7b2b0419b077706b2f71ac5f24531f
Michael Lukowski <lukowski9@gmail.com>
2019-05-02 13:51:23 -0500

    new way of generating primitive roots

--- Diffie-Hellman/generator.py
+++ Diffie-Hellman/generator.py
@@ -2,19 +2,17 @@ import sievePrimes
 import random
 
 def gcd(a,b):
-	while a != b:
-		if a > b:
-			a = a - b
-		else:
-			b = b - a
-	return a
+	if b:
+		return gcd(b, a % b)
+	else:
+		return a;
 
 
 
 def public_prime():
 	# this is wrong we need to generat the prime number and a random primative root from that prime number
 
-	# establish a list of primes and pick one of them as our private key for exchange 
+	# establish a list of primes using sieves method and pick one of them as our private key for exchange 
 	primes = sievePrimes.sievesMethod(200)
 	pub_key = random.choice(primes)
 	return pub_key
@@ -24,8 +22,34 @@ def public_generator():
 	return 0
 
 def primitive_root(modulo):
+	# create a required set to find coprime numbers to the given prime number
 	required_set = set(num for num in range (1, modulo) if gcd(num, modulo) == 1)
 	for g in range(1, modulo):
+		# then we loop through
 		actual_set = set(pow(g, powers) % modulo for powers in range (1, modulo))
 		if required_set == actual_set:
 			return g
+
+def all_primitive_roots(root, prime):
+	roots = []
+	for a in range(1, (prime - 1)):
+		if gcd(a, prime-1) == 1:
+			print("this is the power", pow(root, a))
+			roots.append(pow(root, a))
+	return(roots)
+
+
+def prim_roots(prime):
+	roots = []
+
+	for r in range(2, prime):
+		a = 1
+		k = r % prime
+		while k > 1:
+			a += 1
+			k = k * r
+			k = k % prime
+		if a == (prime -1):
+			roots.append(r)
+
+	return roots
--- Diffie-Hellman/tester.py
+++ Diffie-Hellman/tester.py
@@ -1,13 +1,10 @@
 import generator
-
+import math
 prime = generator.public_prime()
 print("this is our prime: ", prime)
-root = generator.primitive_root(prime)
-print("this is our primative root: ", root)
 
-print("now we are are going to test the primative root function with a static prime")
+print("now we are are going to test the primative root function with a static prime of 23")
 
 static_prime = 23
 
-for i in range(10):
-	print(generator.primitive_root(static_prime))
+print("all of the primitive roots are: ", generator.prim_roots(static_prime))
