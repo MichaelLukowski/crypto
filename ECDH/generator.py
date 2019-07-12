9aea7fde4d4ad31dd042fe0265eb9e017a3f2f72
Michael Lukowski <lukowski9@gmail.com>
2019-07-12 09:13:15 -0500

    implementation of X25519 curve arithmatic

--- ECDH/generator.py
+++ ECDH/generator.py
@@ -1,7 +1,11 @@
 import sievePrimes
 import random
 import math
+import sys
 
+# Establishment of global variables
+P = 2 ** 255 - 19
+A24 = 121665
 
 class ecdhSystem:
 	"""
@@ -18,37 +22,96 @@ class ecdhSystem:
 		self.prime = (2**255) - 19
 		self.a = 486662
 		self.b = 1
-		self.generatorPoint = 9
+		self.gX = 9
+		self.gY = 14781619447589544791020593568409986887264606134616475288964881837755586237401
 		self.order = (2**252) + 27742317777372353535851937790883648493
 		self.cofactor = 8
 
-		
+def decodeLittleEndian(b, bits):
+	return sum([b[i] << 8*i for i in range((bits+7)/8)])
 
-def gcd(a,b):
-	if b:
-		return gcd(b, a % b)
-	else:
-		return a;
+def decodeUCoordinate(u, bits):
+	u_list = [ord(b) for b in u]
+	# Ignore any unused bits.
+	if bits % 8:
+	   u_list[-1] &= (1<<(bits%8))-1
+	return decodeLittleEndian(u_list, bits)
 
-def public_prime():
-	# this is wrong we need to generat the prime number and a random primative root from that prime number
+def encodeUCoordinate(u, bits):
+	u = u % p
+	return ''.join([chr((u >> 8*i) & 0xff) for i in range((bits+7)/8)])
 
-	# establish a list of primes using sieves method and pick one of them as our private key for exchange 
-	primes = sievePrimes.sievesMethod(200)
-	pub_key = random.choice(primes)
-	return pub_key
+def decodeScalar25519(k):
+	k_list = [ord(b) for b in k]
+	k_list[0] &= 248
+	k_list[31] &= 127
+	k_list[31] |= 64
+	return decodeLittleEndian(k_list, 255)
 
-def prim_roots(prime):
-	roots = []
+def cswap(swap, x_2, x_3):
+	dummy = swap * ((x_2 - x_3) % P)
+	x_2 = x_2 - dummy
+	x_2 %= P
+	x_3 = x_3 + dummy
+	x_3 %= P
+	return (x_2, x_3)
 
-	for r in range(2, prime):
-		a = 1
-		k = r % prime
-		while k > 1:
-			a += 1
-			k = k * r
-			k = k % prime
-		if a == (prime -1):
-			roots.append(r)
+def X25519(k, u):
+	x_1 = u
+	x_2 = 1
+	z_2 = 0
+	x_3 = u
+	z_3 = 1
+	swap = 0
 
-	return roots
+	for t in reversed(range(255)):
+		k_t = (k >> t) & 1
+		swap ^= k_t
+		x_2, x_3 = cswap(swap, x_2, x_3)
+		z_2, z_3 = cswap(swap, z_2, z_3)
+		swap = k_t
+
+		A = x_2 + z_2
+		A %= P
+
+		AA = A * A
+		AA %= P
+
+		B = x_2 - z_2
+		B %= P
+
+		BB = B * B
+		BB %= P
+
+		E = AA - BB
+		E %= P
+
+		C = x_3 + z_3
+		C %= P
+
+		D = x_3 - z_3
+		D %= P
+
+		DA = D * A
+		DA %= P
+
+		CB = C * B
+		CB %= P
+
+		x_3 = ((DA + CB) % P)**2
+		x_3 %= P
+
+		z_3 = x_1 * (((DA - CB) % P)**2) % P
+		z_3 %= P
+
+		x_2 = AA * BB
+		x_2 %= P
+
+		z_2 = E * ((AA + (A24 * E) % P) % P)
+		z_2 %= P
+
+	x_2, x_3 = cswap(swap, x_2, x_3)
+	z_2, z_3 = cswap(swap, z_2, z_3)
+
+	return (x_2 * pow(z_2, P - 2, P)) % P
+	
--- ECDH/test.py
+++ ECDH/test.py
@@ -1,12 +1,32 @@
-from generator import ecdgSystem
+from generator import ecdhSystem, X25519, decodeUCoordinate
+from os import urandom
+import random
 
 # testing of the 
 bob = ecdhSystem()
+alice = ecdhSystem()
+
+bob_private_key = random.randint(3 , bob.order-1)
+alice_private_key = random.randint(3 , alice.order-1)
 
 
-print("prime", bob.prime)
-print("a", bob.a)
-print("b", bob.b)
-print("generator", bob.generatorPoint)
-print("order", bob.order)
-print("cofactor", bob.cofactor)
+print("\nBob's Private Key: ")
+print(hex(bob_private_key))
+
+bob_public_key = X25519(bob_private_key, 9)
+print("\nBob's Public Key: ")
+print(hex(bob_public_key))
+
+print("\nAlice's Private Key: ")
+print(hex(alice_private_key))
+
+alice_public_key = X25519(alice_private_key, 9)
+print("\nAlice's Public Key: ")
+print(hex(alice_public_key))
+
+bob_calculated_secret = X25519(bob_private_key, alice_public_key)
+alice_calculated_secret = X25519(alice_private_key, bob_public_key)
+
+print("\nHopefully these 2 lines are the same: ")
+print(hex(bob_calculated_secret))
+print(hex(alice_calculated_secret))
